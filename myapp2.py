4900f959a1a7ace234acd18abbd70e99886c9311
poisollan <mohankumar46456@gmail.com>
2026-03-06 20:55:52 +0530

    new run

--- myapp2.py
+++ myapp2.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return ""Mohan DevOps CI/CD Success""
+    return "Mohan DevOps CI/CD Success"
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
