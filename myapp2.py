c9b208e1ddc13581ef1ef075b5ad3cfdcbe1f35e
poisollan <mohankumar46456@gmail.com>
2026-03-09 13:43:57 +0530

    trigger pipeline

--- myapp2.py
+++ myapp2.py
@@ -1,13 +1,12 @@
-from flask import Flask
 import os
+from flask import Flask
 
 app = Flask(__name__)
 
-ENVIRONMENT = os.environ.get("APP_COLOR", "UNKNOWN")
-
 @app.route("/")
 def home():
-    return f"Active Environment : {ENVIRONMENT}"
+    color = os.getenv("APP_COLOR", "UNKNOWN")
+    return f"<h1>Active Environment : {color}</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
